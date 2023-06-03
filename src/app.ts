6b90bb0f52d29b02c5163b17c5a2fc41068deb42
deveshbajaj3 <devesh.bajaj3@gmail.com>
2023-06-03 10:34:46 +0530

    set ts api

--- src/app.ts
+++ src/app.ts
@@ -4,6 +4,6 @@ const app = express();
 const addTwoNumber = (a: number,b:number):number => a + b;
 app.get('/', (req,res) => {
     const result = addTwoNumber(3,4)
-    res.send('Sum to number :'+result);
+    res.send('Sum to numbers :'+result);
 });
 app.listen(3000, ()=> console.log('server is running'));
